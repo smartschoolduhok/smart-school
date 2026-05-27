1b3f6aa9167b21f706948eb4e83eef400ad00307
ibrahim school <smartschool.duhok@gmail.com>
2026-05-27 20:45:06 +0000

    Phase 10: Task 1 - Project audit cleanup (dead files, archives, duplicates removed)

--- fix_passwords.sql
+++
@@ -1,6 +0,0 @@
-UPDATE users SET password_hash = 'fec40b1064ee33fb17ea5d6741ff7a1f73c56fc789e2ce5dca8a246f934eb0c3' WHERE id = 1;
-UPDATE users SET password_hash = '89ff5406d1bdd9a60014f2fd79e4397897d9bb17c692c48a6164f114fd97a281' WHERE id = 2;
-UPDATE users SET password_hash = '0cd4dc8d9533d88450058f4afdf147dce7f7d1ec0b5daf83c703935ec3ff22ee' WHERE id = 3;
-UPDATE users SET password_hash = '2856c1e528be93bb2c261755eb71bb3e1c1ef9f4c9f4f65031ba28a52de7563a' WHERE id = 4;
-UPDATE users SET password_hash = '80d02588132ccedfc7f6b15e1b162e512a2164fe50e8873fe85039376dd65e17' WHERE id = 5;
-UPDATE users SET password_hash = '6fec04712803c852f0a7cae4ea971a31f24fb0305623894edef5c2b27db30045' WHERE id = 6;
